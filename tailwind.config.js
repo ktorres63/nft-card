8ce134f73dc0b9b2ac1329f32c41f82f94eff78e
ktorres63 <ktorresaro@unsa.edu.pe>
2024-08-14 12:56:36 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "system-ui"],
+      },
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        "very-dark-blue-main": "hsl(217, 54%, 11%)",
+        "very-dark-blue-card": "hsl(216, 50%, 16%)",
+        "very-dark-blue-line": "hsl(215, 32%, 27%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
